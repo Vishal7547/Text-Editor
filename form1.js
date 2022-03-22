6495f42be835ba4fe1d9105b634982895c83b74d
Vishal kumar <87267576+Vishal7547@users.noreply.github.com>
2022-03-23 01:51:37 +0530

    editor

---
+++ form1.js
@@ -0,0 +1,162 @@
+
+
+function bold1(){
+    document.getElementById('variety').style.display="none"
+    document.getElementById('var').style.display="none"
+    document.getElementById('text').style.fontWeight="bold"
+}
+
+function bold11(){
+    document.getElementById('variety').style.display="none"
+    document.getElementById('var').style.display="none"
+    document.getElementById('text').style.fontStyle="italic"
+}
+
+function bold111(){
+    document.getElementById('variety').style.display="none"
+    document.getElementById('var').style.display="none"
+    document.getElementById('text').style.textAlign="left"
+}
+
+function bold1111(){
+    document.getElementById('variety').style.display="none"
+    document.getElementById('var').style.display="none"
+    document.getElementById('text').style.textAlign="right"
+}
+
+function bold11111(){
+    document.getElementById('variety').style.display="none"
+    document.getElementById('var').style.display="none"
+    document.getElementById('text').style.textAlign="center"
+}
+
+function bold111111(){
+    document.getElementById('variety').style.display="none"
+    document.getElementById('var').style.display="none"
+    document.getElementById('text').style.textTransform="uppercase"
+}
+
+function bold11111111(){
+    document.getElementById('variety').style.display="none"
+    document.getElementById('var').style.display="none"
+    document.getElementById('text').style.textTransform="lowercase"
+}
+
+function bold111111111(){
+    document.getElementById('variety').style.display="none"
+    document.getElementById('var').style.display="none"
+    document.getElementById('text').style.textTransform="capitalize"
+}
+
+
+
+
+
+
+
+function run(){
+    document.getElementById('var').style.display="none"
+ document.getElementById('variety').style.display="block"
+}
+function bold0(){
+    document.getElementById('text').style.color="yellow"
+}
+
+
+function bold00(){
+    document.getElementById('text').style.color="pink"
+}
+
+function bold000(){
+    document.getElementById('text').style.color="green"
+}
+
+function bold0000(){
+    document.getElementById('text').style.color="black"
+}
+
+function bold00000(){
+    document.getElementById('text').style.color="blue"
+}
+
+function bold000000(){
+    document.getElementById('text').style.color="red"
+}
+
+function bold0000000(){
+    document.getElementById('text').style.color="#d9d9d9"
+}
+
+function bold00000000(){
+    document.getElementById('text').style.color="orange"
+}
+
+function bold000000000(){
+    document.getElementById('text').style.color="white"
+}
+
+
+
+
+
+function run1(){
+    document.getElementById('variety').style.display="none"
+    document.getElementById('var').style.display="block"
+   }
+   function bold2(){
+       document.getElementById('text').style.backgroundColor="yellow"
+   }
+   
+   
+   function bold22(){
+       document.getElementById('text').style.backgroundColor="pink"
+   }
+   
+   function bold222(){
+       document.getElementById('text').style.backgroundColor="green"
+   }
+   
+   function bold2222(){
+       document.getElementById('text').style.backgroundColor="black"
+   }
+   
+   function bold22222(){
+       document.getElementById('text').style.backgroundColor="blue"
+   }
+   
+   function bold222222(){
+       document.getElementById('text').style.backgroundColor="red"
+   }
+   
+   function bold2222222(){
+       document.getElementById('text').style.backgroundColor="#d9d9d9"
+   }
+   
+   function bold22222222(){
+       document.getElementById('text').style.backgroundColor="orange"
+   }
+   
+   function bold222222222(){
+       document.getElementById('text').style.backgroundColor="#ffffff"
+   }
+
+
+
+
+   function last(){
+    document.getElementById('variety').style.display="none"
+    document.getElementById('var').style.display="none"
+       document.getElementById('text').style.fontWeight="normal"
+       document.getElementById('text').style.fontStyle="normal"
+       document.getElementById('text').style.textTransform="capitalize"
+       document.getElementById('text').style.textAlign="left"
+       document.getElementById('text').style.color="black"
+       document.getElementById('text').style.backgroundColor="white"
+       document.getElementById('text').value= " ";
+   }
+   
+   
+
+
+
+
